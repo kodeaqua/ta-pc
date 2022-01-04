9c3ea4a738d778299021bb0aff77f9607f1fe5dc
kodeaqua <anajmizdn@gmail.com>
2022-01-04 10:23:26 +0700

    Add shell script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+pip install --quiet --use-feature=2020-resolver --disable-pip-version-check --upgrade scikit-image
+wget --quiet -O bola.jpg "https://raw.githubusercontent.com/kodeaqua/ta-pc/main/res/bola.jpg"
+wget --quiet -O boneka.jpg "https://docs.google.com/uc?export=download&id=1rGok7TVnZShF6sFNlTgDhZgEdMLlUH6x"
+wget --quiet -O daun.png "https://docs.google.com/uc?export=download&id=1SiTZGvMF2XXmHW4gPCLTz_KbFvzSArVP"
+wget --quiet -O mobil.jpg "https://docs.google.com/uc?export=download&id=1GS5uIT4gEZsZyOm3M1m9YIV3Z7f7-FcI"
+wget --quiet -O sel.tif "https://docs.google.com/uc?export=download&id=1EL8RO5wTgFV3MI6K0Pa00ZXg_fKR4VSX"
+wget --quiet -O tulisan.jpg "https://docs.google.com/uc?export=download&id=1g6xOlK5XrvHgAJNdHRVHgCwi1YZ8puqX"
