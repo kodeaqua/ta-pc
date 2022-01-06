7dabecdcc2e6dc2cac735a8907546e6e15debb57
kodeaqua <anajmizdn@gmail.com>
2022-01-06 09:44:27 +0700

    Add resources to setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 pip install --quiet --use-feature=2020-resolver --disable-pip-version-check --upgrade scikit-image
 wget --quiet -O bola.jpg "https://raw.githubusercontent.com/kodeaqua/ta-pc/main/res/bola.jpg"
+wget --quiet -O bolaa.jpg "https://raw.githubusercontent.com/kodeaqua/ta-pc/main/res/bolaa.jpg"
+wget --quiet -O bolaaa.jpg "https://raw.githubusercontent.com/kodeaqua/ta-pc/main/res/bolaa.jpg"
 wget --quiet -O boneka.jpg "https://docs.google.com/uc?export=download&id=1rGok7TVnZShF6sFNlTgDhZgEdMLlUH6x"
 wget --quiet -O daun.png "https://docs.google.com/uc?export=download&id=1SiTZGvMF2XXmHW4gPCLTz_KbFvzSArVP"
 wget --quiet -O mobil.jpg "https://docs.google.com/uc?export=download&id=1GS5uIT4gEZsZyOm3M1m9YIV3Z7f7-FcI"
